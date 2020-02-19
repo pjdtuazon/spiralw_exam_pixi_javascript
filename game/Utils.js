394b0112ce2b4ef6130470c4dc464a64d3ca0474
savvycodes <pjdtuazon112@gmail.com>
2020-02-19 09:32:53 +0800

    + updated and added a placeholder for the fire button - a little code cleanup

--- game/Utils.js
+++ game/Utils.js
@@ -6,4 +6,19 @@ class Utils
         return Math.PI * degrees / 180;
     }
     
+    static normalize (target, origin) 
+    {
+        var deltaX = target.x - origin.x;
+        var deltaY = target.y - origin.y;
+        var norm = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
+        
+        if(norm == 0) {
+            return {x:0, y:0};
+        }    
+        
+        var nx = deltaX / norm;
+        var ny = deltaY / norm;
+
+        return {x:nx, y:ny};
+    }
 }
