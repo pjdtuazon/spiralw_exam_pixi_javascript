781059c2383d283d086b58b87c96fc20fc5aa6fc
savvycodes <pjdtuazon112@gmail.com>
2020-02-21 19:20:00 +0800

    + Added Constants file + Added a popup screen + Restarting game is now possible * modified meteor spawn locations +added scoring mode and health bar

---
+++ game/Constants.js
@@ -0,0 +1,16 @@
+
+class Constants
+{
+    static Screen = {width: 480, height: 720};
+    static ScreenCenter = {x: 240, y: 360};
+    static SpawnMeteorWaitTime = 2000;
+    static CruiseSpeed=2.5;
+    static TurnSpeed=5;
+    static MissileSpeed=7;
+    static MeteorMaxSpeed=4;
+    static MinShipMeteorCollisionDist= 70;
+    static MinMissileMeteorCollisionDist=40;
+    static MaxHP = 100;
+    static ScoreIncrement = 10;
+
+}
